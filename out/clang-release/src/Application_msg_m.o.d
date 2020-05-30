ca527a9d76d0d44e652aa008e480133128428370
yasastharinda9511 <yasastharinda9511@gmail.com>
2020-05-30 23:32:36 +0530

    Add application folder to src & start to work on the app1

---
+++ out/clang-release/src/Application_msg_m.o.d
@@ -0,0 +1,6 @@
+../out/clang-release/src/Application_msg_m.o: Application_msg_m.cc \
+  Application_msg_m.h F:/softwares/omnetpp-5.6/include\omnetpp.h
+
+Application_msg_m.h:
+
+F:/softwares/omnetpp-5.6/include\omnetpp.h:
---
+++ out/clang-release/src/applications/app1/app1_m.o.d
@@ -0,0 +1,7 @@
+../out/clang-release/src/applications/app1/app1_m.o: \
+  applications/app1/app1_m.cc applications/app1/app1_m.h \
+  F:/softwares/omnetpp-5.6/include\omnetpp.h
+
+applications/app1/app1_m.h:
+
+F:/softwares/omnetpp-5.6/include\omnetpp.h:
